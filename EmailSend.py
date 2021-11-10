4b817104a4eda7e57448dfb55d0ac2b9f94dbb8b
psxcz10 <psxcz10@nottingham.ac.uk>
2021-11-10 11:51:12 +0000

    Initial commit

---
+++ EmailSend.py
@@ -0,0 +1,107 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+from datetime import date, timedelta
+
+
+class SendEmail:
+    def __init__(self, dailyNum, weekNum, NG7weekNum, dicNews, GBPExchgeRate, GBPExchgeDate):
+
+        self.dailyNum = dailyNum
+        self.weekNum = weekNum
+        self.NG7weekNum = NG7weekNum
+        self.dicNews = dicNews
+        self.GBPExchgeRate = GBPExchgeRate
+        self.GBPExchgeDate = GBPExchgeDate
+        self.hostServer = 'smtp.qq.com'
+        self.mailUser = '984397521@qq.com'
+        self.password = 'pkkmozwrurvdbbad'
+
+        self.receiver = ['psxcz10@nottingham.ac.uk', 'aqxyz4@nottingham.ac.uk', 'enxsl19@nottingham.ac.uk ',
+                         '516874282@qq.com', 'xinyuanfan@outlook.com', '1323850726@qq.com', 'lixyl221@nottingham.ac.uk'
+                         ,'eexxx9@nottingham.ac.uk']
+        # self.receiver = ['psxcz10@nottingham.ac.uk', '984397521@qq.com', 'nuaazcx@163.com']
+        # self.receiver = ['alycl15@exmail.nottingham.ac.uk']
+        self.receiverName = ['Changxin', 'ZHU']
+
+        self.sender = '984397521@qq.com'
+        today = date.today() + timedelta(days=-1)
+        today.ctime()
+        self.todayDate = str(today.day) + '/' + str(today.month) + '/' + str(today.year)
+        self.EmailText()
+
+
+
+    def EmailText(self):
+        Newstitle = []
+        for i in self.dicNews:
+            Newstitle.append(i)
+
+        mailMsg0 = ','.join(self.receiverName)
+        mailMsg1 = f"""<p>Hi, </p><br>
+        <p>Good Morning! This is an automatic email, please don't reply it.</p>
+        <p><u><b>Positive Reported Number</b></u></p>
+        <p>The daily number of tested people positive reported for <i>last 7 days</i> is <b>{self.NG7weekNum}</b> in the 
+        area of <i>NG7 3LP</i>.</p> 
+        <p>The number reported on <i>{self.todayDate}</i> is <b>{self.dailyNum}</b> in Nottingham! 
+        And the positive reported number for <i>last 7 days</i> is <b>{self.weekNum}</b>. </p>
+        <p><u><b>Daily News</b></u></p>
+        <p>You might be intersted in the daily News below (from BBC News):</p>"""
+
+        mailMsg2 = """"""
+        for News in self.dicNews:
+            mailMsg2 += f"""<a href = {self.dicNews[News]}>{News}<a><br>"""
+        mailMsg2 += f"""
+        <p><u><b>Exchange Rate</b></u></p>
+        <p>The current market price for changing 100 <b>GBP</b> directly for <b>RMB</b> is <b>{self.GBPExchgeRate}</b>, 
+        which is published at <i>{self.GBPExchgeDate}</i>(UTC+8). <b>(BANK OF CHINA)</b></p>
+
+        <p>Remember to wear a mask and keep social distance, even if you don't care it. Enjoy your day!</p>
+        <br></br>
+        <p>Best wishes,</p>
+        <p>Changxin ZHU</p>"""
+
+        mailMsg = mailMsg1 + mailMsg2
+        # f"""<a href = {self.dicNews[Newstitle[0]]}>{Newstitle[0]}<a><br>
+        # <a href = {self.dicNews[Newstitle[1]]}>{Newstitle[1]}<a><br>
+        # <a href = {self.dicNews[Newstitle[2]]}>{Newstitle[2]}<a><br>
+        # <a href = {self.dicNews[Newstitle[3]]}>{Newstitle[3]}<a>
+
+        message = MIMEText(mailMsg, 'html', 'utf-8')
+        # message['From'] = Header("菜鸟教程", 'utf-8')
+        # message['To'] = Header("测试", 'utf-8')
+
+        subject = 'Python SMTP 邮件测试'
+        message['Subject'] = Header('Daily hello from Changxin', 'utf-8')
+        message['From'] = ("%s<"+self.sender+">") % (Header(self.sender, 'utf-8'))
+        message['To'] = ','.join(self.receiver)
+        # mailMsg += mailMsg0
+        try:
+            smtpObj = smtplib.SMTP_SSL(self.hostServer)
+            smtpObj.login(self.mailUser, self.password)
+            smtpObj.sendmail(self.sender, message['To'].split(','), message.as_string())
+            print("邮件发送成功")
+        except smtplib.SMTPException:
+            print("Error: 无法发送邮件")
+
+
+'''
+        
+        <p><u><b>The Trip to Oxford</b></u></p>
+        <p>For the mates who are going to visit the Oxford, a series of <b>spots</b> listed would be useful to refer:(you can visit the tickets booking web via the link directly)<br>
+        As a big fan of <b>Harry Potter</b>, the <a href = https://www.chch.ox.ac.uk/plan-your-visit/tickets>Christ Church Cathedral<a> 
+        ,Tom Quad, <a href = https://visit.bodleian.ox.ac.uk/plan-your-visit>Bodleian Libraries<a>(It seems that you can't enter 
+        without a permission, but Divinty School is available) must be in your list.<br><br>
+        Also, the <a href = https://universitychurch.bookinglive.com>University Church of St Mary the Virgin<a> is a recommended
+        spot to enjoy the whole city, and the trip to <a href = https://bookings.oxfordcastleandprison.co.uk/book>Oxford Castle & Prison<a>
+        could be a way of knowing history, <a href = https://goo.gl/maps/PzazDWTia7skLGvq6>Radcliffe Camera<a>, <a href = https://goo.gl/maps/tdB2jujLwV6kxoot6>Bridge of Sighs<a>
+        are both best known symbols of Oxford.<br>
+        <b>Tasty food</b>: <br>
+        <a href = https://goo.gl/maps/4DpKqdfi59DPRwnd9>Covered Market<a><br>
+        British style: <a href = https://goo.gl/maps/T5waifeMjbKyjeLE7>Vaults and Garden<a><br>
+        Thai: <a href = https://goo.gl/maps/seooodZdm7jT9RAb6>Thaikhun<a><br>
+        Ice cream: <a href = https://goo.gl/maps/BcLFq3hUxfhWksmWA>George & Danver<a><br>
+        <b>Shopping</b>:<br>
+        <a href = https://g.page/AliceInWonderlandShop?share>Alice's shop<a><br></p>
+        <p>Hope to have a wonderful trip!</p>
+'''
---
+++ collect.py
@@ -0,0 +1,110 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup as bf
+import requests
+import re
+
+class CollectData():
+    def __init__(self):
+        self.url = "https://coronavirus.data.gov.uk/search?postcode=NG73LP"
+        self.urlNews = "https://www.bbc.co.uk/news/uk"
+        self.urlExchgeRate = "https://www.boc.cn/sourcedb/whpj/"
+        self.process()
+        self.analyze()
+        self.getNews()
+        self.getExchgeRate()
+
+    def process(self):
+
+        # html = urlopen(self.url)
+        # self.htmlText = bytes.decode(html.read())
+        self.r = requests.get(self.url)
+        self.rNews = requests.get(self.urlNews)
+        self.rExchgeRate = requests.get(self.urlExchgeRate)
+        self.rExchgeRate.encoding = 'utf-8'
+
+    def analyze(self):
+        obj = bf(self.r.text, 'lxml')
+        # objText = obj.find('span', attrs={'class': 'govuk-link--no-visited-state number-link number'}).get_text()
+        # objText1 = obj.find('span', attrs={'class': 'govuk-link--no-visited-state number-link'}).get_text()
+
+
+        miniCard = obj.find('li', attrs={'class': 'mini-card'})
+
+        node = miniCard.find('span', attrs={'class': 'govuk-link--no-visited-state number-link number'})
+
+        objText = node.contents[0]
+        node1 = miniCard.find('span', attrs={'class': 'govuk-link--no-visited-state number-link'})
+        objText1 = node1.contents[0]
+
+        NG7Node = obj.find('ul', attrs={'class': 'govuk-list numbers-container govuk-!-margin-top-2'})
+        objText2 =  NG7Node.find('span', attrs={'class': 'govuk-link--no-visited-state number-link number'}).get_text()
+
+        objText = re.sub(r'\t|\n','', objText) #去除制表符
+        # ret0 = re.search(r'D', objText).span()
+        objText1 = re.sub(r'\t|\n','', objText1)
+        # objText1 = ''.join(temp)
+        # print(objText1)
+        # ret1 = re.search(r'N', objText1).span()
+        objText2 = re.sub(r'\t|\n','', objText2)
+        # print(objText2)
+        # print(ret1)
+        # ret1 = re.search(r'[^Daily]+[$2021]', objText).span()
+        print(objText)
+        self.dailyNum = objText
+        self.weekNum = objText1
+        self.NG7weekNum = objText2
+
+        print(self.dailyNum, self.weekNum, self.NG7weekNum)
+
+    def getNews(self):
+        root = "https://www.bbc.co.uk"
+        obj = bf(self.rNews.text, 'lxml')
+        #头条新闻
+        objText1 = obj.find('h3', attrs={
+            'class': 'gs-c-promo-heading__title gel-paragon-bold gs-u-mt+ nw-o-link-split__text'}).get_text()
+        url1 = obj.find('a', attrs={
+            'gs-c-promo-heading gs-o-faux-block-link__overlay-link gel-paragon-bold gs-u-mt+ nw-o-link-split__anchor'}).get(
+            "href")
+        urlNews1 = root + url1
+        self.dicNews = {objText1 : urlNews1}
+        lst1 = obj.find('ul', attrs={'class': 'gel-layout gel-layout--equal nw-c-related-items__list--row'}).find_all('li')
+        for i in lst1:
+            objText1 = i.get_text()
+            url1 = i.contents[0].contents[0].attrs['href']
+            urlNews1 = root + url1
+            self.dicNews[objText1] = urlNews1
+
+        print(self.dicNews)
+
+
+
+
+        # objText2 = obj.find('span', attrs={'class': 'nw-c-related-items__text gs-u-align-bottom'}).get_text()
+        # url2 = obj.find('a', attrs={'nw-o-link-split__anchor gs-u-pt- gs-u-pb- gs-u-display-block nw-o-link-split__text'}).get("href")
+        # urlNews2 = root + url2
+        #
+        # objText3 = obj.find('span', attrs={'class': 'nw-c-related-items__text gs-u-align-bottom'}).get_text()
+        # url3 = obj.find('a', attrs={
+        #     'nw-o-link-split__anchor gs-u-pt- gs-u-pb- gs-u-display-block nw-o-link-split__text'}).get("href")
+        # urlNews3 = root + url2
+        # print(objText2)
+
+
+
+
+        # ret = re.split(r'Daily', objText)
+        # print(ret)
+    def getExchgeRate(self):
+        obj = bf(self.rExchgeRate.text, 'lxml')
+        nodeGBP = obj.find('div', attrs={'class': 'BOC_main'})
+        res = r'(?<=<td>).*?(?=</td>)'
+        exchgeLst = nodeGBP.find_all('td')
+        for i in range(0, len(exchgeLst)):
+            if '英镑' in exchgeLst[i]:
+                GBPnodeInd = i
+        self.GBPExchgeRate = exchgeLst[GBPnodeInd + 3]
+        self.GBPExchgeRate = self.GBPExchgeRate.contents[0]
+        self.GBPExchgeDate = exchgeLst[GBPnodeInd + 6]
+        self.GBPExchgeDate = self.GBPExchgeDate.contents[0]
+
+
