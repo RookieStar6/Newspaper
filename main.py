9113c976e38587b1ad959a52ac7f92ec3611df1f
RookieStar6 <94005108+RookieStar6@users.noreply.github.com>
2022-06-17 20:01:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,7 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from collect import CollectData
 from EmailSend import SendEmail
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    name = "cx"
-    print(f"HELLO {name}")  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # print_hi("PyCharm")
     collectdata = CollectData()
     sendEmail = SendEmail(collectdata.weekNum, collectdata.NG7weekNum, collectdata.dicNews, collectdata.GBPExchgeRate, collectdata.GBPExchgeDate, collectdata.itemDic, collectdata.itemIntro)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
